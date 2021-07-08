843cd1f6b251992e6f80a3b82707b2b8dd5e76d0
anonymanus817 <73923995+Ayaan817@users.noreply.github.com>
2021-07-08 09:46:50 +0530

    Add files via upload

---
+++ dayfromdate.py
@@ -0,0 +1,79 @@
+'''
+
+still not working
+
+'''
+
+from math import floor as fl
+
+year = int(input("Enter YEAR:"))
+rem_years = year - 1
+
+cent_batches = [400,300,200,100]
+
+cent_oddays = [0,1,3,5]
+
+year_oddays = 0
+
+
+
+for cent in cent_batches:
+
+    year_oddays += ((fl(rem_years / cent))*(cent_oddays[cent_batches.index(cent)]))
+    year_oddays %= 7
+    
+    rem_years %= cent
+
+
+if int(str(year)[-1]) == 0 and int(str(year)[-2]) == 0:
+    if year % 400 == 0:
+        leap_year = True
+    else:
+        leap_year = False
+
+else:
+    if year % 4 == 0:
+        leap_year = True
+    else:
+        leap_year = False
+
+
+if leap_year:
+    feb = 1
+else:
+    feb = 0
+
+
+month = int(input("Enter MONTH:"))
+rem_months = month - 1
+
+month_arr = [3,feb,3,2,3,2,3,3,2,3,2,3]
+
+month_oddays = 0
+i = 0
+
+while i != rem_months:
+    for num in range(0,rem_months-1):
+        month_oddays += month_arr[num - 1]
+    i += 1
+
+month_oddays %= 7
+
+date = int(input("Enter DATE:"))
+date_oddays = date % 7
+
+total_oddays = (year_oddays + month_oddays + date_oddays) % 7
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+print(f"{date} {months[month-1]}, {year} is a {days[total_oddays]}.")
+
+    
+
+
+
+
+
+
+    

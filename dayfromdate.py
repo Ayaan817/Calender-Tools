f300548d0bb90d1ca481f38de4a639f020906b9f
anonymanus817 <73923995+Ayaan817@users.noreply.github.com>
2021-07-09 10:04:40 +0530

    finally it works
    but still doesn't handle errors

--- dayfromdate.py
+++ dayfromdate.py
@@ -1,12 +1,7 @@
-'''
-
-still not working
-
-'''
-
 from math import floor as fl
 
 year = int(input("Enter YEAR:"))
+
 rem_years = year - 1
 
 cent_batches = [400,300,200,100]
@@ -20,10 +15,13 @@ year_oddays = 0
 for cent in cent_batches:
 
     year_oddays += ((fl(rem_years / cent))*(cent_oddays[cent_batches.index(cent)]))
-    year_oddays %= 7
-    
     rem_years %= cent
 
+leap_years = fl(rem_years / 4)
+norm_years = rem_years - leap_years
+
+year_oddays += ((2 * leap_years) + norm_years)
+year_oddays %= 7
 
 if int(str(year)[-1]) == 0 and int(str(year)[-2]) == 0:
     if year % 400 == 0:
@@ -44,20 +42,21 @@ else:
     feb = 0
 
 
-month = int(input("Enter MONTH:"))
+month = int(input("Enter MONTH:")) 
 rem_months = month - 1
 
 month_arr = [3,feb,3,2,3,2,3,3,2,3,2,3]
 
 month_oddays = 0
-i = 0
 
-while i != rem_months:
-    for num in range(0,rem_months-1):
-        month_oddays += month_arr[num - 1]
-    i += 1
+n = 0
+
+while n != rem_months:
+    month_oddays += month_arr[n]
+    n += 1
 
 month_oddays %= 7
+
 
 date = int(input("Enter DATE:"))
 date_oddays = date % 7
@@ -69,7 +68,6 @@ months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'Augus
 
 print(f"{date} {months[month-1]}, {year} is a {days[total_oddays]}.")
 
-    
 
 
 
